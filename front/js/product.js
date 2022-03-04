b87c9eb72b30361d8c20ae108a724cb404f37e1c
Kalderonn <mikael.evan@hotmail.fr>
2022-03-04 18:16:44 +0100

    Ajout de condition et commentaires dans  product.js

--- front/js/product.js
+++ front/js/product.js
@@ -20,7 +20,7 @@ const getProductData = async () => {
 
 // getProductData()
 
-// Les éléments HTML
+// Les éléments HTML 
 const productImg = document.querySelector(".item__img");
 const productTitle = document.getElementById("title");
 const productPrice = document.getElementById("price");
@@ -30,9 +30,10 @@ const productOptions = document.getElementById("colors");
 // console.log(productOptions)
 
 // Affichage du produit
-
 const productDisplay = async () => {
+  // récupération des infos de mon produit par l'appel de ma fonction getProductData
   await getProductData();
+  // j'injecte mon HTML
   document.title = productData.name;
   productImg.innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altText} ">`;
   productTitle.innerText = `${productData.name}`;
@@ -49,65 +50,66 @@ productDisplay();
 
 //  Ajout des produits au panier:
 
-// Le panier est un array avec ID du produit, quantité et couleur
-// Utiliser local storage pour garder ses valeurs
-// Faire une condition si le produit n'est pas présent dans l'array, on l'ajoute (ID et couleur)
-// Faire une condition si le produit est déjà présent alors on incrémente la quantité dans l'array
-
-// recupération des evenements au clique du bouton "ajouter au panier"
+// recupération des éléments HTML
 const productQuantity = document.getElementById("quantity");
 const addToBasketBtn = document.getElementById("addToCart");
 
-// sauvegarde du panier
+// Sauvegarde du panier dans le localStorage avec en paramètre le panier que je souhaite enregistrer (ici basket)
 const saveLocalStorage = (basket) => {
   localStorage.setItem("basket", JSON.stringify(basket));
 }
-
+// retourne la valeur associée à la clé basket transformé en objet
 const getLocalStorage = () => {
   let basket = localStorage.getItem("basket");
+  // si le basket n'éxiste pas getItem retourne un null
+  // donc si le panier est vide on retourne un panier vie (tableau vide), s'il n'est pas vide on retoune la valeur associée à la clé basket transformé en objet
   if (basket == null) {
     return [];
   } else {
     return JSON.parse(basket);
   }
 }
-
-const addToLocalStorage = (product) => {
-  let basket = getLocalStorage();
-  basket.push(product);
-  saveLocalStorage(basket);
-}
-
+// Ajout au panier
 const addToBasket = () => {
+  // on écoute les evenements au clic du bouton "ajouter au panier"
   addToBasketBtn.addEventListener(`click`, (e) => {
     console.log(e);
+    // on récupère les valeurs de quantité et couleur
     let quantity = productQuantity.value;
     let color = productOptions.value;
     // console.log(color)
+    // création de l'objet produit qui sera push dans le LocalStorage
     let product = {
       id : addArticleId,
       colorSelected : color,
-      quantitySelected : Number(quantity),
+      quantitySelected : quantity,
     }
+    // récupération du panier
     let basket = getLocalStorage()
-    // Ajout d'une variable contenant le résultat de la recherche
-    const resultFindIdAndColor = basket.find((el) => el.id === addArticleId && el.colorSelected === color);
+    // je rajoute une condition pour ne pas stocker dans le LocalStorage un produit qui à 0 quantité et sans couleur sélectionnée
+    if (quantity == 0 || color == "") {
+      alert('veuillez choisir une quantité et une couleur')
+    } else {
+      // Ajout d'une variable contenant le résultat de la recherche en fonction de la condition
+    const resultFindIdAndColor = basket.find((p) => p.id == addArticleId && p.colorSelected == color);
+    // Si On trouve un produit qui a déjà la même ID et couleur on ajout la nouvelle quantité avec celle déjà présente
     if (resultFindIdAndColor) {
       let newQuantity = parseInt(quantity) + parseInt(resultFindIdAndColor.quantitySelected);
       resultFindIdAndColor.quantitySelected = newQuantity;
-      localStorage.setItem("basket", JSON.stringify(basket));
-      console.log(newQuantity);
-
-    } if (color = "" || quantity == 0) {
-
+      // console.log(newQuantity);
     } else {
-      addToLocalStorage(product)
+      // Je push le product dans le panier = tableau
+      basket.push(product);
     }
-    
+    // Je sauvegarde le panier
+    saveLocalStorage(basket);
+    }
   });
 }
 
 addToBasket();
+
+
 // récupération de la valeur de la couleur
 // productOptions.addEventListener(`change`, (e) =>{
 //   console.log(e.target.value);
